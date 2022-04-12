7b1dfe3672c163f9fbdf9a298654757c0fe6550e
Debaleena Guha <d_guha@Debaleenas-MacBook-Air.local>
2022-04-12 17:17:38 +0530

    username in answersheet

--- src/app/base/admin/report/report.component.ts
+++ src/app/base/admin/report/report.component.ts
@@ -7,6 +7,7 @@ import { Component, Input, OnInit } from "@angular/core";
 })
 export class ReportComponent implements OnInit {
   @Input() reportInfo;
+  @Input() candidateFullName;
   constructor() {}
 
   ngOnInit(): void {}
