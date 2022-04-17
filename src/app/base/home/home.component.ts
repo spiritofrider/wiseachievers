cf5cb1b123a123e492647b3f216cb599d9445da2
Debaleena Guha <d_guha@Debaleenas-MacBook-Air.local>
2022-04-17 14:02:29 +0530

    chnages for loader2

--- src/app/base/home/home.component.ts
+++ src/app/base/home/home.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { BsModalService } from "ngx-bootstrap/modal";
+import { SignupComponent } from "src/app/shared/common/signup/signup.component";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
+  bsModalRef: any;
 
-  constructor() { }
+  constructor(private BsModalService: BsModalService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  consultingSignUp() {
+    this.bsModalRef = this.BsModalService.show(SignupComponent, {
+      backdrop: "static",
+      keyboard: false,
+    });
   }
-
 }
--- src/app/layout/top-layout/top-layout.component.ts
+++ src/app/layout/top-layout/top-layout.component.ts
@@ -1,55 +1,60 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import {BsModalService} from 'ngx-bootstrap/modal';
-import { CommonService } from 'src/app/services/commonservice';
-import { StorageService } from 'src/app/services/storage.service';
-import { LoginComponent } from 'src/app/shared/common/login/login.component';
-import { SignupComponent } from 'src/app/shared/common/signup/signup.component';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { BsModalService } from "ngx-bootstrap/modal";
+import { CommonService } from "src/app/services/commonservice";
+import { StorageService } from "src/app/services/storage.service";
+import { LoginComponent } from "src/app/shared/common/login/login.component";
+import { SignupComponent } from "src/app/shared/common/signup/signup.component";
 
 @Component({
-  selector: 'app-top-layout',
-  templateUrl: './top-layout.component.html',
-  styleUrls: ['./top-layout.component.scss']
+  selector: "app-top-layout",
+  templateUrl: "./top-layout.component.html",
+  styleUrls: ["./top-layout.component.scss"],
 })
 export class TopLayoutComponent implements OnInit {
   bsModalRef: any;
-  username : any ;
-  token : any;
+  username: any;
+  token: any;
 
-  constructor(private BsModalService:BsModalService,private storageService:StorageService,private route:Router,
-    private common : CommonService) { }
+  constructor(
+    private BsModalService: BsModalService,
+    private storageService: StorageService,
+    private route: Router,
+    private common: CommonService
+  ) {}
 
   ngOnInit(): void {
-    this.username = this.common.tokenDecryption(this.storageService.getCookie('token'))['fullName']
-    this.token = this.storageService.getCookie('token')
+    this.username = this.common.tokenDecryption(
+      this.storageService.getCookie("token")
+    )["fullName"];
+    this.token = this.storageService.getCookie("token");
   }
-  openLoginModal(){
-    this.bsModalRef = this.BsModalService.show(LoginComponent,{
-      backdrop : 'static',
-      keyboard: false
-    })
+  openLoginModal() {
+    this.bsModalRef = this.BsModalService.show(LoginComponent, {
+      backdrop: "static",
+      keyboard: false,
+    });
 
-  let loginSubscriber = this.BsModalService.onHide.subscribe(res=>{
-    let username = this.bsModalRef.content.username
-    if(username){
-      loginSubscriber.unsubscribe();
-      this.username = username;
-      this.token = this.storageService.getCookie('token')
-    }
-  })
+    let loginSubscriber = this.BsModalService.onHide.subscribe((res) => {
+      let username = this.bsModalRef.content.username;
+      if (username) {
+        loginSubscriber.unsubscribe();
+        this.username = username;
+        this.token = this.storageService.getCookie("token");
+      }
+    });
   }
 
-  logout(){
-    this.storageService.clearAllCookie()
+  logout() {
+    this.storageService.clearAllCookie();
     this.token = undefined;
-    this.route.navigate(['base/home'])
+    this.route.navigate(["base/home"]);
   }
 
-  openSignupModal(){
-    this.bsModalRef = this.BsModalService.show(SignupComponent,{
-      backdrop : 'static',
-      keyboard: false
-    })
+  openSignupModal() {
+    this.bsModalRef = this.BsModalService.show(SignupComponent, {
+      backdrop: "static",
+      keyboard: false,
+    });
   }
-
 }
